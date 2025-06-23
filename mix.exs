bc41d4ce92e786c8bde2fbb67e5d8a5051c33d9c
alinianitchii <62421086+alinianitchii@users.noreply.github.com>
2025-06-23 14:46:17 +0200

    feat: add LICENSE

--- mix.exs
+++ mix.exs
@@ -6,7 +6,9 @@ defmodule HelixClub.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/alinianitchii/helix_club_ex"}
     ]
   end
 
