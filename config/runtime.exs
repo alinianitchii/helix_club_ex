e5bf90946c06b27e38b4564cdb88aa89c3d62d80
alinianitchii <alin.ianitchii@golee.it>
2025-07-04 19:30:22 +0200

    fix: docker compose database url

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,11 +2,8 @@ import Config
 
 config :logger, level: :debug
 
-config :people, People.Infrastructure.Db.Repo,
-  url: "#{System.fetch_env!("DATABASE_URL")}/people_dev"
+config :people, People.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
 
-config :memberships, Memberships.Infrastructure.Db.Repo,
-  url: "#{System.fetch_env!("DATABASE_URL")}/memberships_dev"
+config :memberships, Memberships.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
 
-config :payments, Payments.Infrastructure.Db.Repo,
-  url: "#{System.fetch_env!("DATABASE_URL")}/payments_dev"
+config :payments, Payments.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
