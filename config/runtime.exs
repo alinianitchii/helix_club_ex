c46232245a45278b30b8b64d3cad3ae4a41f0a35
alinux <alin.yanytskii@gmail.com>
2025-11-13 20:03:54 +0100

    chore: upgrade to latest postgres

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,9 +1,9 @@
 import Config
 
-config :logger, level: :debug
+#config :logger, level: :debug
 
-config :people, People.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
+#config :people, People.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
 
-config :memberships, Memberships.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
+#config :memberships, Memberships.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
 
-config :payments, Payments.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
+#config :payments, Payments.Infrastructure.Db.Repo, url: System.fetch_env!("DATABASE_URL")
